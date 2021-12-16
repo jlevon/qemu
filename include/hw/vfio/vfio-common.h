6db03d8487b19c7ed3ff01a2e7b651247426eada
Jagannathan Raman <jag.raman@oracle.com>
2021-12-16 14:19:05 -0800

    vfio: add region cache
    cache VFIO_DEVICE_GET_REGION_INFO results to reduce
    memory alloc/free cycles and as prep work for vfio-user
    
    Originally-by: John Johnson <john.g.johnson@oracle.com>
    Signed-off-by: Elena Ufimtseva <elena.ufimtseva@oracle.com>
    Signed-off-by: Jagannathan Raman <jag.raman@oracle.com>
    Signed-off-by: John Levon <john.levon@nutanix.com>

--- include/hw/vfio/vfio-common.h
+++ include/hw/vfio/vfio-common.h
@@ -148,6 +148,7 @@ typedef struct VFIODevice {
     IOMMUFDBackend *iommufd;
     VFIOIOASHwpt *hwpt;
     QLIST_ENTRY(VFIODevice) hwpt_next;
+    struct vfio_region_info **regions;
 } VFIODevice;
 
 struct VFIODeviceOps {
